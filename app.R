128f88864a08e80777f82cf5447b79f0f6c42f2d
lheley <lajh87@live.co.uk>
2023-05-17 09:07:34 -0400

    basic app

--- app.R
+++ app.R
@@ -1,10 +1,27 @@
 library(shiny)
+library(shinyAce)
+library(DiagrammeR)
+
+pool <- connect_db()
+onStop(function(){pool::poolClose(pool)})
 
 ui <- fluidPage(
-  
+  column(
+    width = 4,
+    aceEditor("ace", "digraph{a->b}", "dot")
+  ),
+  column(
+    width = 8,
+    grVizOutput("graph")
+  )
 )
 
 server <- function(input, output, session) {
+  
+  output$graph <- renderGrViz({
+    grViz(input$ace)
+  })
+
   
 }
 
