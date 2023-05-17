7b2c321f464614ead2fe04a1ba38f83973482a43
lheley <lajh87@live.co.uk>
2023-05-17 11:59:08 -0400

    add ability to load a saved graph

---
+++ R/loadGraph.R
@@ -0,0 +1,37 @@
+loadModal <- function(){
+  showModal(
+    modalDialog(
+      title = "Load a Saved Graph", size = "m",
+      DT::DTOutput("graphtbl"),
+      footer = tagList(
+        actionButton("load_graph", "Load"),
+        modalButton("Close")
+      ),
+      easyClose = TRUE,
+      FADE = TRUE
+    )
+  )
+}
+
+graphDT <- function(db){
+  DT::datatable(db |>
+                  dplyr::tbl("graphviz") |>
+                  dplyr::select(id, label) |>
+                  dplyr::collect(),
+                selection = "single",
+                options = list(dom = "ftp",
+                               autoWidth = TRUE,
+                               columnDefs = list(
+                                 list(width = '25px', targets = c(0)),
+                                 list(width = '500px', targets = c(1))
+                               )),
+                rownames = FALSE
+  )
+}
+
+getGraph <- function(db, selected_id){
+  new_graph <- db |>
+    dplyr::tbl("graphviz") |>
+    dplyr::filter(.data$id == selected_id) |>
+    dplyr::pull(graph)
+}
--- app.R
+++ app.R
@@ -1,15 +1,16 @@
 library(shiny)
 library(shinyAce)
 library(DiagrammeR)
-# https://stackoverflow.com/questions/70381005/how-to-add-controls-in-panzoom-functionality-in-shiny-app
-# pool <- connect_db()
-# onStop(function(){pool::poolClose(pool)})
+
+pool <- connect_db()
+onStop(function(){pool::poolClose(pool)})
 
 ui <- fluidPage(
   loadPanzoom("graph"),
   column(
     width = 4,
-    aceEditor("ace", "digraph{a->b}", "dot")
+    actionButton("load", "Load"),
+    aceEditor(outputId = "ace",value = NULL ,mode =  "dot")
   ),
   column(
     width = 8,
@@ -22,10 +23,31 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  output$graph <- renderGrViz({
-    grViz(input$ace)
+  # Connect to database and set initial values
+  db <- pool::poolCheckout(pool)
+  values <- reactiveValues(graph = "digraph{a->b}")
+  
+  # Display graph and observe change to editor
+  output$graph <- renderGrViz(grViz(input$ace))
+  
+  observe({
+    updateAceEditor(
+      session, 
+      "ace", 
+      values$graph
+    )
   })
-
+  
+  # Load Graph ----
+  observeEvent(input$load,loadModal())
+  output$graphtbl <- DT::renderDataTable(graphDT(db))
+   
+  observeEvent(input$load_graph,{
+    values$graph <- getGraph(db, input$graphtbl_rows_selected)
+  })
+  
+  # Return checkout connection object
+  onStop(function() pool::poolReturn(db))
 }
 
 shinyApp(ui, server)
